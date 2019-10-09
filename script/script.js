3779abcff2d39369d270c38e20de1a495830fc9c
lordSerega <51482894+lordSerega@users.noreply.github.com>
2019-10-09 20:53:28 +0300

    Обновление клавиатуры
    Добавлена кнопка смены языка.
    Изменена реализация клавиатуры.

--- script/script.js
+++ script/script.js
@@ -7,26 +7,62 @@ document.addEventListener("DOMContentLoaded",() => {
         const keyboard = document.querySelector(".keyboard");
         const closeKeyboard = document.getElementById("close-keyboard");
         const searchInput = document.querySelector(".search-form__input");
-        const space = document.getElementById("space");
-        const backspace = document.getElementById("keyboard-backspace");
-        
+
+
         const toggleKeyboard = () => {
              keyboard.style.top = keyboard.style.top ? "" : "50%";
         };
 
+        const changeLang = (btn, lang) => {
+          const langRu = [
+            'ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+            'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',
+            'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',
+            'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.',
+            'en', ' '
+          ];
+          const langEn = [
+            '`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"',
+            'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+            'ru', ' '
+          ];
+
+          if (lang === 'en'){
+            btn.forEach((elem, i) => {
+              elem.textContent = langEn[i];
+
+            })
+          } else {
+              btn.forEach((elem, i) => {
+                elem.textContent = langRu[i];
+            })
+          }
+        }
         const typing = event => {
-            const target = event.target;
+          const target = event.target;
 
-            if (target === backspace) {
-                searchInput.value = searchInput.value.substring(0, searchInput.value.length - 1);
-            } else if (target === space) {
-                searchInput.value += ' ';
-            } else if (target.tagName === "BUTTON") {
-                searchInput.value += target.textContent.trim();
+          if (target.tagName.toLowerCase() === "button") {
+            const buttons = [...keyboard.querySelectorAll("button")]
+              .filter(elem => elem.style.visibility !== "hidden");
+            console.dir(buttons);
+            const contentButton = target.textContent.trim();
+            if (contentButton === "⬅") {
+              searchInput.value = searchInput.value.slice(0,-1);
+            } else if (!contentButton) {
+              searchInput.value += " ";
+            } else if (contentButton === "en" ||
+            contentButton === "ru") {
+              changeLang(buttons,contentButton)
+            } else {
+              searchInput.value += contentButton;
             }
+            }
+          }
 
-            }
-   
+
+
         keyboardButton.addEventListener("click", toggleKeyboard);
         closeKeyboard.addEventListener("click", toggleKeyboard);
         keyboard.addEventListener("click", typing);
@@ -37,7 +73,7 @@ document.addEventListener("DOMContentLoaded",() => {
 {
    const burger = document.querySelector('.spinner');
    const sidebarMenu = document.querySelector('.sidebarMenu');
-   
+
    burger.addEventListener("click", () => {
        burger.classList.toggle('active');
        sidebarMenu.classList.toggle('rollUp');
